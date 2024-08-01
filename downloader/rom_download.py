8afcf3f75fdacc26fb7b1a1d35a9f37970d0a3f8
danieldiasm <daniel_diasm@hotmail.com>
2024-08-01 21:02:30 +0200

    ROM Download Constructor

--- downloader/rom_download.py
+++ downloader/rom_download.py
@@ -1,1 +1,12 @@
 # ROM Download Class
+
+class ROM:
+
+    def __init__(self, title, rom_page_url, download_path) -> None:
+        self.title = title
+        self.rom_page_url = rom_page_url
+        self.download_url = None
+        self.download_status = False
+        self.download_path = download_path
+        self.bs4_page = None
+        self.game_data = {}
