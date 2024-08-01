58dcdd4b3be851726a2c33ec7d27cb619d3a866b
danieldiasm <daniel_diasm@hotmail.com>
2024-08-02 00:02:28 +0200

    Introduced class and methods for url extractor

--- downloader/url_extractor.py
+++ downloader/url_extractor.py
@@ -4,31 +4,30 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+class Page:
+
+    def __init__(self, page_url) -> None:
+        self.page_url = page_url
+        self.download_url = None
+        self.bs4_page = None
+        self.re_pattern = 'window\.open\(".*", "_self"\);'
+
+    def get_url_data(self):
+        return requests.get(self.page_url, headers={'User-Agent': 'Mozilla/5.0'})
+    
+    def get_bs4_page(self):
+        url_data = self.get_url_data()
+        self.bs4_page = BeautifulSoup(url_data, "html.parser")
+
+    def extract_download_url(self):
+        self.get_bs4_page()
+        page_script = self.get_bs4_page.find_all("script").__repr__()
+        link = re.search('window\.open\(".*", "_self"\);', page_script).group()
+        start_index = link.find('"') + 1
+        end_index = link[start_index].find('"') + start_index
+        self.download_url = link[start_index:end_index]
+
+
+
 # Have the target URL
-url = "https://coolrom.com.au/roms/psx/39719/Tekken_3.php"
-
-result = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-
-# Create a new BS object
-# soup = BeautifulSoup(result, "html.parser")
-soup = BeautifulSoup(result.content, "html.parser")
-
-# Retrieve only the "script" tag sections of the page text calling the repr method
-page_script = soup.find_all("script").__repr__()
-
-# Then clean up taking only the part that shows window.open( bla bla bla until "_self");
-# Note that we had to escape special characters used by the regex
-download_link = re.search('window\.open\(".*", "_self"\);',page_script).group()
-
-# Then we remove what doesn't matter to us, we want just the generated link
-start_index = download_link.find('"') + 1
-end_index = download_link[start_index:].find('"') + start_index
-
-# Finally extract the link
-result = download_link[start_index:end_index]
-
-# This is the result
-print(download_link[start_index:end_index])
-
-# It can be converted into a class with methods for the extraction
+# url = "https://coolrom.com.au/roms/psx/39719/Tekken_3.php"
