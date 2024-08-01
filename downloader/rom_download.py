2d6529f8051524c41978d351b56dd43c3a7a9acd
danieldiasm <daniel_diasm@hotmail.com>
2024-08-01 22:30:17 +0200

    Added methods, getters and setters

--- downloader/rom_download.py
+++ downloader/rom_download.py
@@ -3,6 +3,7 @@
 class ROM:
 
     def __init__(self, title, rom_page_url, download_path) -> None:
+        self.id = None
         self.title = title
         self.rom_page_url = rom_page_url
         self.download_url = None
@@ -10,3 +11,30 @@ class ROM:
         self.download_path = download_path
         self.bs4_page = None
         self.game_data = {}
+    
+    def extract_download_url(self):
+        pass
+
+    def extract_game_data(self):
+        pass
+
+    def download_rom(self):
+        pass
+
+    def make_hash(self):
+        pass
+
+    def set_download_state(self):
+        pass
+
+    def get_download_state(self):
+        pass
+
+    def get_download_url(self):
+        pass
+
+    def get_download_title(self):
+        pass
+
+    def get_download_id(self):
+        pass
