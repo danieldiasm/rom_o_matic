ef2c07013415056b1fbc2e565869bec3c1a13b41
danieldiasm <daniel_diasm@hotmail.com>
2024-08-02 15:05:19 +0200

    added data extraction logic

--- downloader/data_extractor.py
+++ downloader/data_extractor.py
@@ -1,1 +1,19 @@
 # Data Extractor
+
+class GameData:
+
+    @staticmethod
+    def extract(bs4_page):
+        game_data = {}
+        
+        tables = bs4_page.td.table.find_all('b')
+
+        for b in tables:
+            if b.text == 'Game:':
+                game_data['title'] = b.next_element.next_element.next_element.contents[0]
+            if b.text == 'Genre:':
+                game_data['genre'] = b.next_element.next_element.next_element.next_element.contents[0]
+            if b.text == 'System:':
+                game_data['system'] = b.next_element.next_element.next_element.next_element.contents[0]
+
+        return game_data
